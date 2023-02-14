01718bb488ab2a847d7d9ddf3901ae14048a1c20
Julio <julio18@biyuyo.org>
2023-02-14 12:16:16 -0400

    Add routes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import diaryRouter from "./routes/diaries";
 
 const app = express();
 app.use(express.json());
@@ -6,11 +7,12 @@ app.use(express.json());
 const PORT = 3000;
 
 app.get('/', (_req, res) => {
-    console.log("Hello World");
+    console.log("Hello World " + new Date().toLocaleDateString());
     res.send("Hellow World")
 });
 
+app.use("/api/diaries", diaryRouter);
+
 app.listen(PORT, () => {
     console.log("Listening on port " + PORT);
-    //console.log("Listening on port ${PORT}");
 }); 
